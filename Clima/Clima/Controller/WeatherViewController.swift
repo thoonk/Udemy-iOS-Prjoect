5a4844c6cc3c54102816ce28a260a67bc8f4ec94
thoonk <dkrmrm878@gmail.com>
2020-11-30 22:53:01 +0900

    Add Clima Project

---
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+//        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text{
+            weatherManager.fetchWeather(cityName: city)
+        }
+   
+        searchTextField.text = ""
+    }
+    
+}
+
---
+++ WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by 김태훈 on 2020/11/29.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
