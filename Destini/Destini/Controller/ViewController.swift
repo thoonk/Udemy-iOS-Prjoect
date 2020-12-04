7c14f1dc54fd8e9538793ba742e8016c9daf7d10
thoonk <dkrmrm878@gmail.com>
2020-12-04 23:46:56 +0900

    Add Destini Project

---
+++ Destini/Destini/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+   
+    var storyBrain = StoryBrain()
+    
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        storyBrain.setStoryNum(userAnswer)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+}
+
