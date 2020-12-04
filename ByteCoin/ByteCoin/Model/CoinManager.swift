756607deb9217e18d566a27008726a69a58de114
thoonk <dkrmrm878@gmail.com>
2020-12-04 18:57:51 +0900

    Add requestAPI & parseJSON

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -8,15 +8,64 @@
 
 import Foundation
 
+
+protocol CoinManagerDelegate {
+    func didUpdateCoin(_ coinManager: CoinManager, coin: CoinData)
+    func didFailWithError(_ error: Error)
+}
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "apikey"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    var delegate: CoinManagerDelegate?
 
     func getCoinPrice(for currency: String) {
-        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        performRequest(with: urlString)
     }
     
+    func performRequest(with urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    //print(String(data: safeData, encoding: .utf8))
+                    if let coin = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateCoin(self, coin: coin)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ coinData: Data) -> CoinData? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try! decoder.decode(CoinData.self, from: coinData)
+            let assetIdBase = decodedData.assetIdBase
+            let assetIdQuote = decodedData.assetIdQuote
+            let rate = decodedData.rate
+            let time = decodedData.time
+            
+            let coin = CoinData(assetIdBase: assetIdBase, assetIdQuote: assetIdQuote, time: time, rate: rate)
+            return coin
+            
+        } catch {
+            delegate?.didFailWithError(error)
+            return nil
+        }
+    }
 }
