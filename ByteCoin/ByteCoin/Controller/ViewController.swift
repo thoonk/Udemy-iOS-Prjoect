fe538b5c8211cf95f4df5d8a0fb6a3166b9ec23a
thoonk <dkrmrm878@gmail.com>
2020-12-04 17:43:29 +0900

    Add ByteCoin Project

---
+++ ByteCoin/ByteCoin/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currnecyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    let coinManager = CoinManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+
+
+}
+
