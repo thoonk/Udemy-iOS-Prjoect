a7b9d4c8d896512ef781b1e818794b868c829a33
thoonk <dkrmrm878@gmail.com>
2020-12-12 23:46:03 +0900

    Add MessageCell

---
+++ FlashChat/FlashChat/Views/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  FlashChat
+//
+//  Created by 김태훈 on 2020/12/12.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
