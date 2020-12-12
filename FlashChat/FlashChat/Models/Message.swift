02fe402e0ddfc25512c522de12cc0eae02c07f38
thoonk <dkrmrm878@gmail.com>
2020-12-12 23:45:51 +0900

    Add Message

---
+++ FlashChat/FlashChat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by 김태훈 on 2020/12/12.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
