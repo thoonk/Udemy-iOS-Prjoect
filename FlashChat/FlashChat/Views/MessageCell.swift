fd81e5df2616a25a56690155e829ebcbbded8438
thoonk <dkrmrm878@gmail.com>
2020-12-21 16:14:14 +0900

    Add leftImageView

--- FlashChat/FlashChat/Views/MessageCell.swift
+++ FlashChat/FlashChat/Views/MessageCell.swift
@@ -13,7 +13,7 @@ class MessageCell: UITableViewCell {
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var label: UILabel!
-    
+    @IBOutlet weak var leftImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
